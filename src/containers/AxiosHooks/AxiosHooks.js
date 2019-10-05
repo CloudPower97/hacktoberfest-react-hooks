9f0b3e90cafec322470dc024bc564702771fceef
Claudio Cortese <claudio.cortese@outlook.it>
2019-10-05 23:39:27 +0200

    feat: more code

--- src/containers/AxiosHooks/AxiosHooks.js
+++ src/containers/AxiosHooks/AxiosHooks.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import { Switch, Route, Redirect, Link } from 'react-router-dom'
+import { useList, useUpdateEffect } from 'react-use'
 import Star from '../../assets/emoji/star.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faArrowRight, faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 import useAxios from 'axios-hooks'
 
 const AxiosHook = ({ history }) => {
-  const [{ data, error, loading }, refetch] = useAxios('https://jsonplaceholder.typicode.com/posts')
-  const [{ data: manualData, error: manualError, loading: manualLoading }] = useAxios('', {
-    manual: true,
-  })
+  const [{ data: posts, error: errorPosts, loading: loadingPosts }, refetchPosts] = useAxios(
+    'https://jsonplaceholder.typicode.com/posts'
+  )
+  const [{ data: users, error: errorUsers, loading: loadingUsers }, fetchUser] = useAxios(
+    'https://jsonplaceholder.typicode.com/users',
+    {
+      manual: true,
+    }
+  )
+
+  const [list, { set, push, clear, updateAt, remove, filter, sort }] = useList()
+
+  useUpdateEffect(() => {
+    set(users)
+  }, [users])
 
   return (
     <div className="boring-class-component-wrapper">
@@ -45,18 +57,72 @@ const AxiosHook = ({ history }) => {
         }}>
         Next! <FontAwesomeIcon icon={faArrowRight} />
       </Link>
-      <pre
-        style={{
-          fontSize: 'x-large',
-        }}>
-        {JSON.stringify(data)}
-      </pre>
+      {loadingUsers && <p>Loading Users</p>}
+      {users && (
+        <ul>
+          {list.map(({ name }, index) => (
+            <li key={index} className="bullet">
+              <span
+                style={{
+                  color: 'white',
+                }}>
+                {name}
+              </span>
+            </li>
+          ))}
+        </ul>
+      )}
       <div className="boring-class-component-btn-wrapper">
         <button
           style={{
             width: '25%',
+          }}
+          onClick={refetchPosts}>
+          Refetch Posts
+          <FontAwesomeIcon icon={faPlusCircle} />
+        </button>
+        <button
+          style={{
+            width: '25%',
+          }}
+          onClick={fetchUser}>
+          Fetch Users
+          <FontAwesomeIcon icon={faPlusCircle} />
+        </button>
+        <button
+          onClick={() => {
+            push(new Date().toDateString())
+          }}
+          style={{
+            width: '25%',
           }}>
-          Refetch
+          Push new element
+          <FontAwesomeIcon icon={faPlusCircle} />
+        </button>
+
+        <button
+          onClick={clear}
+          style={{
+            width: '25%',
+          }}>
+          Clear all elements
+          <FontAwesomeIcon icon={faPlusCircle} />
+        </button>
+
+        <button
+          onClick={() => filter(user => user.name[0] === 'C')}
+          style={{
+            width: '25%',
+          }}>
+          Filter elements
+          <FontAwesomeIcon icon={faPlusCircle} />
+        </button>
+
+        <button
+          style={{
+            width: '25%',
+          }}>
+          Sort elements
           <FontAwesomeIcon icon={faPlusCircle} />
         </button>
       </div>
